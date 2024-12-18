6127a38a0f4850d766f57870d058003dc0db83da
maeng555 <mjy5188@naver.com>
2024-12-19 00:42:20 +0900

    객체 인스턴스변수 생성

---
+++ week1/src/main/java/org/example/Book.java
@@ -0,0 +1,10 @@
+package org.example;
+ //제목,가격, 저자 ,페이지수, isbn,출판사  .....
+public class Book {
+    public String title;
+    public int price;
+    public String company;
+    public String author;
+    public int page;
+    public String isbn;
+} //새로운자료형 - 하나의 구조 하나의6개속성구조
