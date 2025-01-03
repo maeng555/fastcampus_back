0d65a589c5fda6d47a93e681f70da64829502665
maeng555 <mjy5188@naver.com>
2025-01-03 21:25:42 +0900

    spring io 설정

---
+++ pastapp/src/main/java/com/example/pastapp/PastappApplication.java
@@ -0,0 +1,13 @@
+package com.example.pastapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PastappApplication.class, args);
+	}
+
+}
---
+++ pastapp/src/test/java/com/example/pastapp/PastappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pastapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
